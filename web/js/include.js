96e8fd87437ffdc330370f410d49516335acc29d
Jerry.Yao <grokyao@163.com>
2019-11-25 23:02:56 +0800

    2019-11-25搭建框架

---
+++ web/js/include.js
@@ -0,0 +1,17 @@
+﻿
+document.write("<script language=javascript src='https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js'></script>");//国内cdn
+//document.write("<script language=javascript src='http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.min.js'></script>");//正式服务器迁移到全球cdn
+//layuijs库
+document.write("<script language=javascript src='/layuiadmin/layui/layui.js'></script>");
+
+
+//http操作库
+document.write("<script language=javascript src='/js/jshttp.js?v=" + new Date().getTime() + "'></script>");
+//json操作库
+document.write("<script language=javascript src='/js/json2.js?v=" + new Date().getTime() + "'></script>");
+//cookie操作库
+document.write("<script language=javascript src='/js/jscookie.js?v=" + new Date().getTime() + "'></script>");
+//权限处理库
+document.write("<script language=javascript src='/js/permisson.js?v=" + new Date().getTime() + "'></script>");
+
+
---
+++ web/js/permisson.js
@@ -0,0 +1,7 @@
+﻿
+var userid = getCookie("CurrentUserId");
+
+if (userid == null) {
+    alert("您的登录已过期，请重新登录！");
+    window.location.href = '/RedirectLogin.htm';
+}
